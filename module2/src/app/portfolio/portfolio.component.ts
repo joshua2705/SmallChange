9da10288aee01237541d2a932a9d7259ff75745d
rakshitha <rakshithamurali8@gmail.com>
2022-09-14 14:05:23 +0530

    Portfolio Use case - Functionality

--- module2/src/app/portfolio/portfolio.component.ts
+++ module2/src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Stock } from '../models/stock';
+import { PortfolioService } from '../services/portfolio.service';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
-  constructor() { }
+  portfolio:Stock[] = []
+  invested:number = 0;
+  current:number=0;
+  totalReturns:number=0;
+  constructor(private portfolioService: PortfolioService) { }
 
   ngOnInit(): void {
+    this.getPortfolio();
+    let i=0;
+    for( i=0; i<this.portfolio.length; i++){
+        this.invested += this.portfolio[i].totalInvestment;
+        this.current += this.portfolio[i].priceChange;
+    }
+    this.totalReturns = this.invested + this.current
+
   }
+
+  getPortfolio() {
+     this.portfolio= this.portfolioService.getPortFolio()
+  }
+
+
+
 
 }
