9770048f2932a0107c79d1a205abca27c1fb8d57
Sasmithaa Srinivasan <sasmithaasrinivasan@gmail.com>
2022-09-14 09:52:50 +0530

    stock class

--- module2/src/app/models/stock.ts
+++ module2/src/app/models/stock.ts
@@ -1,2 +1,7 @@
 export class Stock {
+    constructor(public symbol: string, public quantity: string, public orderDate: string,
+        public transactionDate: string, public exgPrice: number, 
+        public type: string ){
+
+    }
 }
