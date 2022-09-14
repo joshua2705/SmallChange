e35a5d9c14daba85cf165855b830144f0dbd3bc1
Alexander,Joshua <joshua.alexander2705@gmail.com>
2022-09-14 14:58:37 +0530

    Trade Modal

--- module2/src/app/trading/trading.component.ts
+++ module2/src/app/trading/trading.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalStock } from '../models/global-stock';
 import { GlobalstockService } from '../services/globalstock.service';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-trading',
@@ -10,10 +11,33 @@ import { GlobalstockService } from '../services/globalstock.service';
 export class TradingComponent implements OnInit {
 
   stocks:GlobalStock[] = []
-  constructor(private stockService:GlobalstockService) { }
+  selectedStock:GlobalStock = new GlobalStock("","",0,0,0)
+  closeResult:string = ""
+  fund:number=234000
+  thisStock:number=500
+  constructor(private stockService:GlobalstockService,private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.stocks = this.stockService.getGlobalStocks()
+  }
+
+  open(content:any, stock:GlobalStock) {
+    this.selectedStock = stock
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
