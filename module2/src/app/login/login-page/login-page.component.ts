9531297f78364c557c259a14f95712efa2bc6f0d
mahvish <mahvish2306@gmail.com>
2022-10-03 10:09:30 +0530

    authentication

--- module2/src/app/login/login-page/login-page.component.ts
+++ module2/src/app/login/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
 import { LoginService } from 'src/app/services/login.service';
 @Component({
   selector: 'app-login-page',
@@ -16,29 +17,37 @@ export class LoginPageComponent implements OnInit {
   }
   
 
-    password:string = ""
-    username:string = ""
+  username: string = ""
+  password: string = ""
+  users: User[] = []
+  authenticated: boolean = false;
 
-  storePassword(e:any):void{
-    this.password = e
+  displayUserName(value: string){
+    this.username = value;
   }
 
-  storeUsername(e:any):void{
-    this.username = e
+  displayPassword(value: string){
+    this.password = value; 
   }
 
-  login(): void {
-
-    if(this.username === undefined || this.password === undefined) {
-      console.log("Please Enter Valid Inputs") 
-    } 
-    else{
-      this.router.navigateByUrl('/portfolio');
-      console.log({Username:this.username, password:this.password, EncodedUsername: window.btoa(this.username), EncodedPassword : window.btoa(this.password) }) 
-      this.username ="";
-      this.password ="";
-    } 
+  getUsers(){
+    this.loginService.getUsers().subscribe(incomingData => this.users=incomingData)
   }
-  
+
+  public authenticate(): void {  
+    this.users.forEach(creds => {
+      if(creds.password === this.password && creds.username === this.username){
+        this.authenticated = true;
+      }
+      else{
+        this.router.navigateByUrl('login')
+      }
+
+
+    });
+    if(this.authenticated==true){
+      this.router.navigateByUrl('portfolio');
+    }
+  }
 
 }
--- module2/src/app/services/login.service.ts
+++ module2/src/app/services/login.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-
+import { User } from '../models/user';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  users = [{username:"Joshua",password:"Awesome"},{username:"Admin",password:"Admino"}]
+  users:User[] = [{username:"Joshua",password:"Awesome"},{username:"Admin",password:"Admino"}]
   constructor() { }
-  getUsers():Observable<[]>{
-    return (this.users)
+
+  getUsers():Observable<User[]>{
+    return of(this.users)
   }
 }
