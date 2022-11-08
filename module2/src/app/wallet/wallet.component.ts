a37fa0d264da7e2d6249bbec27237ff42041253f
Sasmithaa Srinivasan <sasmithaasrinivasan@gmail.com>
2022-11-08 15:13:26 +0530

    started integrating wallet

--- module2/src/app/wallet/wallet.component.ts
+++ module2/src/app/wallet/wallet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WalletService } from '../services/walletservice';
 
 @Component({
   selector: 'app-wallet',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wallet.component.css']
 })
 export class WalletComponent implements OnInit {
+  balance: number = -1;
+  investedAmount: number = - 1;
 
-  constructor() { }
+  constructor(
+    private walletService: WalletService
+  ) { }
 
   ngOnInit(): void {
+    this.getBalance();
+  }
+
+  getBalance(){
+    this.walletService.getBalance().subscribe(
+      data => {
+        console.log(data.balance);
+        this.balance = data.balance;
+        this.investedAmount = data.investedamount;
+
+      }
+    );
+
+  }
+
+  withdrawMoney(value: any){
+    console.log(value);
+
   }
 
 }
