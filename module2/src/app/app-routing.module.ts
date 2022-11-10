b165be4ce5cd56ba61649dbbd3a98c53c5878971
fidzulu <fidzulu@gmail.com>
2022-11-10 09:54:40 +0530

    preference page

--- module2/src/app/app-routing.module.ts
+++ module2/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginPageComponent } from './login/login-page/login-page.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PreferencesComponent } from './preferences/preferences.component';
 import { RegisterFormComponent } from './registration/registration.component';
 import { TradeHistoryComponent } from './trade-history/trade-history.component';
 import { TradingComponent } from './trading/trading.component';
@@ -15,7 +16,8 @@ const routes: Routes = [
   {path:'activity', component: TradeHistoryComponent},
   {path:'trade', component: TradingComponent},
   {path:'login', component: LoginPageComponent},
-  {path:'home', component: LandingPageComponent}
+  {path:'home', component: LandingPageComponent},
+  {path: 'preferences', component: PreferencesComponent}
 ];
 
 @NgModule({
