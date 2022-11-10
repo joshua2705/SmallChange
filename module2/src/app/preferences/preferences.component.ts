cb7110163c3e18ca117b6f5823207617bbe23e6c
fidzulu <fidzulu@gmail.com>
2022-11-10 10:16:23 +0530

    preference page start

--- module2/src/app/preferences/preferences.component.ts
+++ module2/src/app/preferences/preferences.component.ts
@@ -7,7 +7,19 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ['./preferences.component.css']
   })
   export class PreferencesComponent implements OnInit {
+
+    availableStocks: any[] = [
+        {"id":"S01","name":"ABC","price":120},
+        {"id":"S02","name":"ABC","price":130},
+        {"id":"S03","name":"ABC","price":80},
+        {"id":"S04","name":"ABC","price":40}
+    ] ;
+    preferenceStocks: any[] = [
+        {"id":"S03","name":"ABC","price":80},
+        {"id":"S04","name":"ABC","price":40}
+    ];
+
     ngOnInit(): void {
-        throw new Error("Method not implemented.");
+        
     }
 }
